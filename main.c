e738ef36dfb620598154393a738b2ff2da049058
kumsquare <158168882+kumsquare@users.noreply.github.com>
2024-08-04 16:59:07 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+void dfs(int a[10][10],int n, int visited [10],int source)
+{
+    int i;
+    visited [source]=1;
+    for(i=0;i<=n;i++)
+    {
+        if(a[source][i]==1&&visited[i]==0)
+            dfs(a,n,visited,i);
+    }
+}
+int main()
+{
+    int n,a[10][10],i,j,visited[10],count=0;
+    printf("\nEnter number of nodes:\n");
+    scanf("%d",&n);
+    printf("\nRead adjacency matrix:\n");
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=n;j++)
+            scanf("%d",&a[i][j]);
+    }
+    for(i=0;i<=n;i++){
+        visited[i]=0;
+    }
+    for(i=0;i<=n;i++)
+    {
+        if(visited[i]==0)
+        {
+            dfs(a,n,visited,i);
+            count++;
+        }
+    }
+    if(count==1)
+        printf("\nGraph connected 1 component\n");
+    else
+        printf("\nGraph not connected %d component\n",count);
+    return 0;
+}
